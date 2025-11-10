b4853aae735bd3052300b2d716e8deee066f58bf
mku11 <93253421+mku11@users.noreply.github.com>
2025-11-10 19:35:21 +0200

    added encoders

---
+++ src/java/simple-io/com/mku/encode/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Encoders.
+ */
+package com.mku.encode;
---
+++ src/python/simple_io/simple_io/encode/__init__.py
@@ -0,0 +1,6 @@
+"""!@brief Encoders
+"""
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__) + '/../../../simple_io')
